a338c41e0f238c6650aaeba22b327b24a9236f13
Cristian Mocanu <mocanu.cristian93@gmail.com>
2022-03-24 06:51:04 +0200

    test 3

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => res.send("Hello there!"));
 app.get("/1", (req, res) => res.send({test:"just a test bro!"}));
 app.get("/2", (req, res) => res.send({test:"just a test bro 22!"}));
+app.get("/3", (req, res) => res.send({test:"just a test bro 3!"}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
